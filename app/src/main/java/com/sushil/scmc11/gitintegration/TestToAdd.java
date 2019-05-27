a33108042358b26087a2c8438f3f9d02ebc6711f
SushilBhure <34593229+SushilBhure@users.noreply.github.com>
2019-05-27 16:34:19 +0530

    Added new method

--- app/src/main/java/com/sushil/scmc11/gitintegration/TestToAdd.java
+++ app/src/main/java/com/sushil/scmc11/gitintegration/TestToAdd.java
@@ -3,4 +3,8 @@ package com.sushil.scmc11.gitintegration;
 public class TestToAdd {
 
     // demo class file
+    
+    private void testMethod(){
+     //added new method                         
+    }
 }
