f8afc552109d59be2d1c5488dde30d8396d180c0
sachinja76@gmail.com <chetan05>
2019-05-27 15:25:06 +0530

    Initial commit

---
+++ app/src/main/java/com/sushil/scmc11/gitintegration/MainActivity.java
@@ -0,0 +1,20 @@
+package com.sushil.scmc11.gitintegration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    private void pushMethod(){
+
+        Log.e("Test","To push project on Github");
+    }
+}
